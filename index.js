01a1213ca02caf55c2011dd11f650c6db685682c
Roswin216 <roswin2162010@gmail.com>
2026-04-22 01:48:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { Client } = require('discord.js-selfbot-v13');
+const { Client, RichPresence } = require('discord.js-selfbot-v13');
 const client = new Client({ checkUpdate: false });
 const http = require('http');
 
-// Bắt buộc lấy Port do Render tự cấp, nếu test ở máy thì dùng 3000
+// Bắt buộc lấy Port do Render tự cấp
 const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
@@ -15,18 +15,20 @@ http.createServer((req, res) => {
 client.on('ready', async () => {
   console.log(`${client.user.username} đang giả lập VS Code!`);
 
-  client.user.setActivity('Visual Studio Code', {
-    type: 'PLAYING',
-    details: 'Editing server.yml',
-    state: 'Workspace: vps-rs',
-    timestamps: { start: Date.now() },
-    assets: {
-      large_image: 'mp:external/3Y9X-0D-V0pXoUv6704C88LgYwY/https/raw.githubusercontent.com/visualstudio/brand/master/icons/vscode/vscode.png',
-      large_text: 'Visual Studio Code',
-      small_image: 'mp:external/3Y9X-0D-V0pXoUv6704C88LgYwY/https/raw.githubusercontent.com/visualstudio/brand/master/icons/vscode/vscode.png',
-      small_text: 'VS Code'
-    }
-  });
+  // Sử dụng RichPresence và Application ID chuẩn của VS Code
+  const r = new RichPresence(client)
+    .setApplicationId('383226320970055681') // ID hệ thống của VS Code
+    .setType('PLAYING')
+    .setName('Visual Studio Code')
+    .setState('Workspace: vps-rs')
+    .setDetails('Editing server.yml')
+    .setStartTimestamp(Date.now())
+    .setAssetsLargeImage('vscode') // Tên logo có sẵn trên hệ thống Discord
+    .setAssetsLargeText('Visual Studio Code')
+    .setAssetsSmallImage('vscode')
+    .setAssetsSmallText('VS Code');
+
+  client.user.setActivity(r);
 });
 
 client.login(process.env.TOKEN);

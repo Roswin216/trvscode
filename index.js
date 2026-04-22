e0c2ee228dbfcf4d7d84bf06a040e9aabeb500b1
Roswin216 <roswin2162010@gmail.com>
2026-04-22 22:00:45 +0700

    Update server response and log messages

--- index.js
+++ index.js
@@ -2,26 +2,24 @@ const { Client } = require('discord.js-selfbot-v13');
 const client = new Client({ checkUpdate: false });
 const http = require('http');
 
-// Giữ cho Render luôn chạy 24/7
+// Server giữ bot sống trên Render
 const PORT = process.env.PORT || 10000;
-http.createServer((req, res) => {
-  res.write("Bot is running!");
-  res.end();
-}).listen(PORT);
+http.createServer((req, res) => { res.write("Bot is alive!"); res.end(); }).listen(PORT);
 
 client.on('ready', async () => {
-  console.log(`${client.user.username} đã sẵn sàng với App ID mới!`);
+  console.log(`${client.user.username} đang treo 24/7...`);
 
   client.user.setActivity('Visual Studio Code', {
     type: 'PLAYING',
-    applicationId: '1496345858965241988', // ID ứng dụng của Thắng
+    applicationId: '1496345858965241988', // ID của Thắng
     details: 'Editing server.yml',
-    state: 'Workspace: vps-rs',
+    // Giả lập dòng chữ Codespaces giống trong ảnh bạn gửi
+    state: 'Workspace: vps-rs [Codespaces: genial capybara]', 
     timestamps: { start: Date.now() },
     assets: {
-      large_image: 'vsc_large', // Tên ảnh to bạn đã đặt trong Art Assets
+      large_image: 'vsc_large', // Tên ảnh đã lưu trong Graphic Resources
       large_text: 'Visual Studio Code',
-      small_image: 'vsc_small', // Tên ảnh nhỏ bạn đã đặt trong Art Assets
+      small_image: 'vsc_small',
       small_text: 'Developer'
     }
   });
